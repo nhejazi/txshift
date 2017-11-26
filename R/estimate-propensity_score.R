2c50e16036a0620443faea74e84dc735324fbe48
Nima Hejazi <nh@nimahejazi.org>
2017-11-25 20:23:34 -0800

    add SL density fit sketch

--- R/estimate-propensity_score.R
+++ R/estimate-propensity_score.R
@@ -7,6 +7,8 @@
 #' @param A A \code{numeric} vector of observed treatment values.
 #' @param W A \code{numeric} matrix of observed baseline covariate values.
 #' @param delta A \code{numeric} ...
+#' @param fit_type A \code{character} specifying whether to use Super Learner to
+#'  fit the density estimation.
 #' @param ...
 #'
 #' @importFrom condensier fit_density predict_probability
@@ -21,15 +23,41 @@
 est_g <- function(A,
                   W,
                   delta = 0,
+                  fit_type = "standard",
                   ...) {
+
     # make data object
     data_O <- as.data.frame(cbind(A, W))
     if (!is.matrix(W)) W <- as.matrix(W)
     colnames(data_O) <- c("A", paste0("W", seq_len(ncol(W))))
 
     # fit conditional density with condensier
-    fit_g_A <- condensier::fit_density(X = c(paste0("W", seq_len(ncol(W)))),
-                                       Y = "A", input_data = data_O, ...)
+    if (fit_type != "sl") {
+        fit_g_A <- condensier::fit_density(X = c(paste0("W", seq_len(ncol(W)))),
+                                           Y = "A", input_data = data_O, ...)
+    } else if (fit_type == "sl") {
+        bin_est_lib <- list(Lrnr_glm_fast, Lrnr_xgboost, Lrnr_xgboost)
+        bin_estimator_args_in <- list(list(family = "binomial"),
+                                      list(nrounds = 50,
+                                           objective = "reg:logistic"),
+                                      list(nrounds = 50,
+                                           objective = "reg:logistic")
+                                     )
+        built_bin_ests <- list()
+        for (i in seq_len(bin_estimator_args_in)) {
+            built_bin_ests[[i]] <- sl3::make_learner(bin_est_lib[[i]],
+                                                     bin_estimator_args_in[[i]])
+        }
+        condensier_args_in <- list(list(nbins = 25, bin_method = "equal.len",
+                                        pool = TRUE, bin_estimator = NULL),
+                                   list(nbins = 20, bin_method = "equal.mass",
+                                        pool = TRUE, bin_estimator = NULL),
+                                   list(nbins = 35, bin_method = "equal.len",
+                                        pool = TRUE, bin_estimator = NULL)
+                                  )
+    } else {
+        stop("Inappropriate fit_type specified. Please fix, then try again.")
+    }
 
     # predict probabilities for the un-shifted data (A = a)
     pred_g_A_noshift <- condensier::predict_probability(model_fit = fit_g_A,
