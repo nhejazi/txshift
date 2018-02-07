3b2c620bcef680a4017a3c2064c789e8941971f9
David Benkeser <benkeser@berkeley.edu>
2018-02-07 16:01:51 -0500

    some fixes

--- R/tmle-eif_ipcw.R
+++ R/tmle-eif_ipcw.R
@@ -31,42 +31,53 @@ tmle_eif_ipcw <- function(fluc_fit_out,
                           Delta,
                           ipc_weights = rep(1, length(Y)),
                           tol_eif = 1e-7) {
-  # compute TMLE
-  dat <- data.table::as.data.table(cbind(data_in, fluc_fit_out$Qn_shift_star,
-                                         eps_updated$qn[eps_updated$qn != 0]))
-  data.table::setnames(dat, c("W", "A", "Y", "Qn", "qn"))
-  dat_eif <- dat %>%
-    dplyr::add_count(W, round(A, 2))
+  # # compute TMLE
+  # dat <- data.table::as.data.table(cbind(data_in, fluc_fit_out$Qn_shift_star,
+  #                                        eps_updated$qn[eps_updated$qn != 0]))
+  # data.table::setnames(dat, c("W", "A", "Y", "Qn", "qn"))
+  # dat_eif <- dat %>%
+  #   dplyr::add_count(W, round(A, 2))
 
-  # find the contributions to Psi from the unique observations
-  psi_uniq <- dat %>%
-    dplyr::add_count(W, round(A, 2)) %>%
-    dplyr::filter(n == 1) %>%
-    dplyr::select(Qn, qn) %>%
-    dplyr::transmute(
-      psi_uniq_obs = Qn * qn
-    ) %>%
-    unlist() %>%
-    as.numeric()
+  # # find the contributions to Psi from the unique observations
+  # psi_uniq <- dat %>%
+  #   dplyr::add_count(W, round(A, 2)) %>%
+  #   dplyr::filter(n == 1) %>%
+  #   dplyr::select(Qn, qn) %>%
+  #   dplyr::transmute(
+  #     psi_uniq_obs = Qn * qn
+  #   ) %>%
+  #   unlist() %>%
+  #   as.numeric()
 
-  # find the contributions to Psi from the non-unique observations
-  psi_nonuniq <- dat %>%
-    dplyr::add_count(W, round(A, 2)) %>%
-    dplyr::filter(n > 1) %>%
-    dplyr::select(Qn, qn, n) %>%
-    dplyr::transmute(
-      # let's divide by n here since, on average, this'll catch the contribution
-      # as though the observation were unique (???)
-      psi_nonuniq_obs = (Qn * qn) / n
-    ) %>%
-    unlist() %>%
-    as.numeric()
-  dat_eif$eif <- rep(NA, nrow(dat))
-  dat_eif[dat_eif$n == 1, ]$eif <- eif_uniq
-  dat_eif[dat_eif$n > 1, ]$eif <- eif_nonuniq
+  # # find the contributions to Psi from the non-unique observations
+  # psi_nonuniq <- dat %>%
+  #   dplyr::add_count(W, round(A, 2)) %>%
+  #   dplyr::filter(n > 1) %>%
+  #   dplyr::select(Qn, qn, n) %>%
+  #   dplyr::transmute(
+  #     # let's divide by n here since, on average, this'll catch the contribution
+  #     # as though the observation were unique (???)
+  #     psi_nonuniq_obs = (Qn * qn) / n
+  #   ) %>%
+  #   unlist() %>%
+  #   as.numeric()
+  # dat_eif$eif <- rep(NA, nrow(dat))
+  # dat_eif[dat_eif$n == 1, ]$eif <- eif_uniq
+  # dat_eif[dat_eif$n > 1, ]$eif <- eif_nonuniq
 
-  param_obs_est[Delta == 1] <- ipc_weights * fluc_fit_out$Qn_shift_star
-  psi <- sum(param_obs_est)
+  # param_obs_est[Delta == 1] <- ipc_weights * fluc_fit_out$Qn_shift_star
+  # psi <- sum(param_obs_est)
+  # find duplicated rows of data_in (removing "Y" column)
+  rm_col <- "Y"
+  dups <- duplicated(data_in[ , !rm_col, by = key(data_in), with = FALSE])
+  duplicated_idx <- which(dups)
+
+  # if no duplicates duplicated_idx == integer(0)
+  if(length(duplicated_idx) == 0){
+    psi <- sum(fluc_fit_out$Qn_shift_star * eps_updated$qn[ipc_weights > 0])
+  }else{
+    psi <- sum(fluc_fit_out$Qn_shift_star[-duplicated_idx] * eps_updated$qn[-duplicated_idx])
+  }
 
   # compute the efficient influence function (EIF) / canonical gradient (D*)
   eif <- rep(0, length(Delta))
