b350a9b3851c32681c9257a3e228e5cc298e1787
Nima Hejazi <nh@nimahejazi.org>
2018-12-19 22:56:00 -0800

    robustify hazard structure creation

--- R/estimate-density_hal.R
+++ R/estimate-density_hal.R
@@ -2,16 +2,18 @@
 #'
 #' description
 #'
-#' @param args...
+#' @param fold ...
+#' @param long_data ...
+#' @param ... ...
 #'
 #' @importFrom origami training validation
 #'
 #' @export
 #
-cv_haldensify <- function(fold, long_hazard_data, ...) {
+cv_haldensify <- function(fold, long_data, ...) {
   # make training and validation folds
-  train_set <- origami::training(long_hazard_data)
-  valid_set <- origami::validation(long_hazard_data)
+  train_set <- origami::training(long_data)
+  valid_set <- origami::validation(long_data)
 
   # do stuff with HAL
 }
@@ -20,21 +22,30 @@ cv_haldensify <- function(fold, long_hazard_data, ...) {
 #'
 #' description
 #'
-#' @param args...
+#' @param A ...
+#' @param W ...
+#' @param ... ...
 #'
 #' @importFrom origami make_folds cross_validate
 #'
 #' @export
 #
-haldensify <- function(long_hazard_data) {
+haldensify <- function(A, W, ...) {
+  # re-format input data into long hazards structure
+  long_data <- format_long_hazards(A = A, W = W, n_bins = 10)
+
   # extract observation-level IDs and drop column
-  subj_ids <- long_hazard_data$id
-  long_hazard_data[, id := NULL]
+  obs_ids <- long_data$obs_id
+  long_data[, obs_id := NULL]
 
   # make folds with origami
-  folds <- origami::make_folds(long_hazard_data, cluster_ids = subj_ids)
+  folds <- origami::make_folds(long_data, cluster_ids = obs_ids)
 
   # call cross_validate on cv_density function...
+  cv_haldensity <- origami::cross_validate(cv_fun = cv_haldensify,
+                                           folds = folds,
+                                           long_data = long_data,
+                                           ...)
 }
 
 #' Generate long format hazards data for conditional density estimation
@@ -43,45 +54,62 @@ haldensify <- function(long_hazard_data) {
 #'
 #' @param A ...
 #' @param W ...
+#' @param type ...
 #' @param n_bins ...
+#' @param width ...
 #'
 #' @importFrom data.table as.data.table rbindlist setnames
+#' @importFrom ggplot2 cut_interval cut_number cut_width
 #' @importFrom assertthat assert_that
 #'
 #' @export
 #
-format_long_hazards <- function(A, W, n_bins = 10) {
+format_long_hazards <- function(A, W,
+                                type = c("equal_range", "equal_mass",
+                                         "equal_width"),
+                                n_bins = 10, width = NULL) {
+  # clean up arguments
+  type <- match.arg(type)
+
   # get lower and upper limits of A
   bounds <- range(A)
 
   # set grid along A and find interval membership of observations along grid
-  grids <- seq(from = bounds[1], to = bounds[2], length.out = n_bins)
-  bin_id <- findInterval(A, grids)
+  if (type == "equal_range") {
+    bins <- ggplot2::cut_interval(A, n_bins, right = FALSE)
+    bin_id <- as.numeric(bins)
+  } else if (type == "equal_mass") {
+    bins <- ggplot2::cut_number(A, n_bins, right = FALSE)
+    bin_id <- as.numeric(bins)
+  } else {
+    assertthat::assert_that(!is.null(width))
+    bins <- ggplot2::cut_width(A, width, closed = "left")
+    bin_id <- as.numeric(bins)
+  }
 
   df_all_obs <- vector("list", length(A))
   for (i in seq_along(A)) {
-    # create matrix of upper and lower interval limits
-    bins <- matrix(grids[c(1:2, 2:3, 3:4, 4:5, 5:6, 6:7, 7:8, 8:9, 9:n_bins)],
-                   ncol = 2, byrow = TRUE)
+    # create repeating bin IDs for this subject (these map to intervals)
+    all_bins <- matrix(seq_along(levels(bins)), ncol = 1)
 
     # create indicator and "turn on" indicator for interval membership
-    bin_indicator <- rep(0, nrow(bins))
+    bin_indicator <- rep(0, nrow(all_bins))
     bin_indicator[bin_id[i]] <- 1
-    id <- rep(i, nrow(bins))
+    id <- rep(i, nrow(all_bins))
 
     # get correct value of baseline variables and repeat along intervals
     if (is.null(dim(W))) {
       # assume vector
-      obs_w <- rep(W[i], nrow(bins))
+      obs_w <- rep(W[i], nrow(all_bins))
       names_w <- "W"
     } else {
       # assume two-dimensional array
-      obs_w <- rep(as.numeric(W[i, ]), nrow(bins))
+      obs_w <- rep(as.numeric(W[i, ]), nrow(all_bins))
       obs_w <- matrix(obs_w, ncol = ncol(W), byrow = TRUE)
 
       # use names from array if present
       if (is.null(names(W))) {
-        names_w <- paste("W", 1:ncol(W), sep = "_")
+        names_w <- paste("W", seq_len(ncol(W)), sep = "_")
       } else {
         names_w <- names(W)
       }
@@ -90,20 +118,16 @@ format_long_hazards <- function(A, W, n_bins = 10) {
     # create data table with membership indicator and interval limits
     suppressWarnings(
       hazards_df <- data.table::as.data.table(cbind(id, bin_indicator,
-                                                    bins, obs_w))
+                                                    all_bins, obs_w))
     )
 
     # give explicit names and add to appropriate position in list
-    data.table::setnames(hazards_df, c("id", "haz_ind", "lower", "upper",
-                                       names_w))
+    data.table::setnames(hazards_df, c("obs_id", "in_bin", "bin_id", names_w))
     df_all_obs[[i]] <- hazards_df
   }
 
   # combine observation-level hazards data into larger structure
   out <- data.table::rbindlist(df_all_obs)
-
-  # check that output object has the correct dimensionality and return
-  assertthat::assert_that(nrow(out) == length(A) * (length(grids) - 1))
   return(out)
 }
 
